d1fe47b58271ce520dc710718a6e906dcc045d9c
Ilya <ilaivanov97021@gmail.com>
2024-12-05 20:13:43 +0300

    added unit test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,29 +3,30 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    Calculator calculator = new Calculator();
 
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        assertEquals(4, calculator.add(3, 1));
     }
 
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        assertEquals(2, calculator.dif(6, 4));
     }
 
     @Test
     void div() {
-        //TODO inser your realisation in test-method div
+        assertEquals(4, calculator.div(8, 2));
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        assertEquals(6, calculator.times(3, 2));
     }
 
     @Test
     void solver() {
-        //TODO inser your realisation in test-method solver
+        assertEquals(24, calculator.solver());
     }
 }
